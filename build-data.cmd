673b03d158df609e48fc495b092d2ce816f4e3ac
unknown <qianhenglian@gmail.com>
2023-12-27 21:27:03 +0800

    first copy:

---
+++ build-data.cmd
@@ -0,0 +1,5 @@
+cmd/c ptk ptk
+cmd/c ptk ptk ddjxq-bh
+cmd/c ptk ptk ddjxq-comm
+cmd/c ptk ptk ddjxq-pre
+pause
---
+++ src/appstore.js
@@ -0,0 +1,20 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::ddjxq',
+_ACC23.AppVer='23.12.27';
+_ACC23.AppName='ddjxq'
+_ACC23.AppTitle='道德經句讀新詮'
+_ACC23.allptks=['ddjxq','ddjxq-bh','ddjxq-pre','ddjxq-comm'];
+_ACC23.selectedptks=['ddjxq','ddjxq-bh','ddjxq-pre','ddjxq-comm']
+_ACC23.activeptk='ddjxq'
+_ACC23.ptkinfo={
+    'ddjxq':"文言",
+    'ddjxq-bh':"白話",
+    'ddjxq-pre':"準備",
+    'ddjxq-comm':"新詮",
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
